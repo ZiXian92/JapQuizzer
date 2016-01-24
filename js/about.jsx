bc3dae22b32915b5eb97c57f438aaed8391cdf51
zixian92 <zixian1992@hotmail.com>
2016-01-24 22:09:25 +0800

    Touches up About page

--- js/about.jsx
+++ js/about.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const About = React.createClass({
   render(){
     return (
-      <div className="white-text">This is about page</div>
+      <div id="about-content" className="white-text">
+        <h3>Hi, I am Zi Xian. This site was created during Hack n Roll 2016 after wasting much time thinking of ideas and then realising that
+        I have yet to prepare for my Japanese language vocabulary quiz which is on the next day. Hope this will be useful in helping you
+        memorise Japanese language better.</h3>
+      </div>
     );
   }
 });
